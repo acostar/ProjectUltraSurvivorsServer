a5738e7813f70f8b0efc712c9de7c735c2c22463
acostar <acrueda99@gmail.com>
2025-04-06 15:11:52 +0200

    Added leaderBoard control and update

--- server/main.go
+++ server/main.go
@@ -6,38 +6,90 @@ import (
 	"log"
 )
 
-type message struct {
-	UserID 	string `json:"userID"`
-	Data	string `json:"data"`
+type gameRun struct {
+	UserID		string `json:"userID"`
+	Log		string `json:"log"`
+	ReachedLevel	int `json:"reachedLevel"`
+	EnemiesKilled	int `json:"enemiesKilled"`
+	Score		int `json:"score"`
 }
 
-var messages = []message{
-	{ UserID: "1", Data: "[LevelLogger] LEVEL0-----" },
-	{ UserID: "2", Data: "[LevelLogger] LEVEL0-----[LevelLogger] LEVEL1-----" },
-	{ UserID: "3", Data: "[LevelLogger] LEVEL0-----[LevelLogger] LEVEL1-----[LevelLogger] LEVEL2-----[LevelLogger] LEVEL3-----[LevelLogger] LEVEL4-----[LevelLogger] LEVEL5-----" },
+type gameRunLeaderBoard struct {
+	UserID		string `json:"userID"`
+	ReachedLevel	int `json:"reachedLevel"`
+	EnemiesKilled	int `json:"enemiesKilled"`
+	Score		int `json:"score"`
 }
+
+var gameRuns = []gameRun{
+	{ UserID: "PollaGorda123", Log: "[LevelLogger] LEVEL0-----", ReachedLevel: 4, EnemiesKilled: 5, Score: 512 },
+	{ UserID: "PutoAmoMCPene", Log: "[LevelLogger] LEVEL0-----", ReachedLevel: 5, EnemiesKilled: 7, Score: 1024 },
+	{ UserID: "MarioKartA90Pavos", Log: "[LevelLogger] LEVEL0-----", ReachedLevel: 6, EnemiesKilled: 10, Score: 2048 },
+}
+
+var leaderBoard []gameRunLeaderBoard
 
 func main() {
 	router := gin.Default()
-	router.GET("/messages", getMessages)
-	router.POST("/messages", postMessages)
+	router.GET("/leaderBoard", getLeaderBoard)
+	router.POST("/gameRun", postGameRun)
+
+	leaderBoard = appendGameRunToLeaderBoard(gameRuns[0])
 
 	router.Run("localhost:8000")
 }
 
-func getMessages(c *gin.Context) { c.IndentedJSON(http.StatusOK, messages) }
+func getLeaderBoard(c *gin.Context) {
+	updateLeaderBoard()
+	c.IndentedJSON(http.StatusOK, leaderBoard) 
+}
 
-func postMessages(c *gin.Context) {
-	log.SetPrefix("postMessages: ")
+func postGameRun(c *gin.Context) {
+	log.SetPrefix("PostGameRun: ")
 	log.SetFlags(0)
 
-	var newMessage message
+	var newGameRun gameRun
 
-	if err := c.BindJSON(&newMessage); err != nil { 
+	if err := c.BindJSON(&newGameRun); err != nil { 
 		log.Fatal(err)
-		return 
+		return
 	}
 
-	messages = append(messages, newMessage)
-	c.IndentedJSON(http.StatusCreated, newMessage)
+	gameRuns = append(gameRuns, newGameRun)
+	c.IndentedJSON(http.StatusCreated, newGameRun)
+}
+
+func updateLeaderBoard() {
+	for _, gameRun := range gameRuns {
+		userFound := false
+		for i, gameRunLeaderBoard := range leaderBoard {
+			if gameRun.UserID == gameRunLeaderBoard.UserID {
+				if gameRun.ReachedLevel > gameRunLeaderBoard.ReachedLevel {
+					leaderBoard[i] = updateGameRunLeaderBoard(gameRunLeaderBoard, gameRun)
+				}
+				userFound = true
+				break
+			}
+		}
+		
+		if !userFound { leaderBoard = appendGameRunToLeaderBoard(gameRun) }
+	}
+}
+
+func appendGameRunToLeaderBoard(gameRun gameRun) []gameRunLeaderBoard {
+	return append(leaderBoard, gameRunLeaderBoard{
+		UserID: gameRun.UserID,
+		ReachedLevel: gameRun.ReachedLevel,
+		EnemiesKilled: gameRun.EnemiesKilled,
+		Score: gameRun.Score,
+	})
+}
+
+func updateGameRunLeaderBoard(actualGameRunLeaderBoard gameRunLeaderBoard, gameRun gameRun) gameRunLeaderBoard {
+	return gameRunLeaderBoard{
+		UserID: gameRun.UserID,
+		ReachedLevel: gameRun.ReachedLevel,
+		EnemiesKilled: gameRun.EnemiesKilled,
+		Score: gameRun.Score,
+	}
 }
